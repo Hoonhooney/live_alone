368c07708172fade09a84f2c7d9fdabad4ff1c55
kante <kante@popone.co.kr>
2019-04-14 10:09:35 +0900

    메인화면에 임시로 홈피드로 가는 버튼 추가

--- app/src/main/java/com/example/kante/live_alone/MainActivity.java
+++ app/src/main/java/com/example/kante/live_alone/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.kante.live_alone;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.text.TextUtils;
 import android.util.Log;
-import com.example.kante.live_alone.R;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,15 +17,17 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.ActionCodeSettings;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import android.support.design.widget.Snackbar;
 import com.google.firebase.auth.FirebaseAuthActionCodeException;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
 
-
-
+/**
+ * Demonstrate Firebase Authentication without a password, using a link sent to an
+ * email address.
+ */
 public class MainActivity extends BaseActivity implements View.OnClickListener {
-    private static final String TAG = "EMAIL_LINK_AUTHENTICATION";
+
+    private static final String TAG = "PasswordlessSignIn";
     private static final String KEY_PENDING_EMAIL = "key_pending_email";
 
     private FirebaseAuth mAuth;
@@ -45,6 +47,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
 
         mSendLinkButton = findViewById(R.id.passwordlessSendEmailButton);
@@ -53,7 +56,6 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
 
         mEmailField = findViewById(R.id.fieldEmail);
         mStatusText = findViewById(R.id.status);
-
 
         mSendLinkButton.setOnClickListener(this);
         mSignInButton.setOnClickListener(this);
@@ -68,6 +70,19 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         // Check if the Intent that started the Activity contains an email sign-in link.
         checkIntent(getIntent());
 
+        Button goHomeFeed = findViewById(R.id.btn_homefeed);
+        goHomeFeed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn_gohome();
+            }
+        });
+    }
+
+    public void btn_gohome(){
+        Intent intent = new Intent(this, HomeFeed.class);
+        startActivity(intent);
+        finish();
     }
 
     @Override
@@ -84,19 +99,12 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
             findViewById(R.id.mainFields).setVisibility(View.VISIBLE);
             findViewById(R.id.passwordlessButtons).setVisibility(View.VISIBLE);
             findViewById(R.id.signedInButtons).setVisibility(View.GONE);
-            if(user.isEmailVerified()){
-                Intent intent = new Intent(this,
-                        HomeFeed.class);
-                startActivity(intent);
-                finish();
-            }
         } else {
             findViewById(R.id.mainFields).setVisibility(View.VISIBLE);
             findViewById(R.id.passwordlessButtons).setVisibility(View.VISIBLE);
             findViewById(R.id.signedInButtons).setVisibility(View.GONE);
         }
     }
-
     @Override
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
@@ -109,12 +117,19 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         outState.putString(KEY_PENDING_EMAIL, mPendingEmail);
     }
 
+    /**
+     * Check to see if the Intent has an email link, and if so set up the UI accordingly.
+     * This can be called from either onCreate or onNewIntent, depending on how the Activity
+     * was launched.
+     */
     private void checkIntent(@Nullable Intent intent) {
         if (intentHasEmailLink(intent)) {
             mEmailLink = intent.getData().toString();
+
             mStatusText.setText(R.string.status_link_found);
             mSendLinkButton.setEnabled(false);
             mSignInButton.setEnabled(true);
+
         } else {
             mStatusText.setText(R.string.status_email_not_sent);
             mSendLinkButton.setEnabled(true);
@@ -136,36 +151,9 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         return false;
     }
 
-
-    private void showSnackbar(String message) {
-        Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.passwordlessSendEmailButton:
-                onSendLinkClicked();
-                break;
-            case R.id.passwordlessSignInButton:
-                onSignInClicked();
-                break;
-            case R.id.signOutButton:
-                onSignOutClicked();
-                break;
-        }
-    }
-
-    private void onSendLinkClicked() {
-        String email = mEmailField.getText().toString();
-        if (TextUtils.isEmpty(email)) {
-            mEmailField.setError("Email must not be empty.");
-            return;
-        }
-
-        sendSignInLink(email);
-    }
-
+    /**
+     * Send an email sign-in link to the specified email.
+     */
     private void sendSignInLink(String email) {
         ActionCodeSettings settings = ActionCodeSettings.newBuilder()
                 .setAndroidPackageName(
@@ -173,14 +161,14 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
                         false, /* install if not available? */
                         null   /* minimum app version */)
                 .setHandleCodeInApp(true)
-                .setUrl("https://www.naver.com/")
+                .setUrl("https://auth.example.com/emailSignInLink")
                 .build();
 
         hideKeyboard(mEmailField);
         showProgressDialog();
 
-        final String email_new=email;
-        mAuth.sendSignInLinkToEmail(email_new, settings)
+        final String email_new = email;
+        mAuth.sendSignInLinkToEmail(email, settings)
                 .addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
@@ -205,16 +193,10 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
                 });
     }
 
-    private void onSignInClicked() {
-        String email = mEmailField.getText().toString();
-        if (TextUtils.isEmpty(email)) {
-            mEmailField.setError("Email must not be empty.");
-            return;
-        }
-
-        signInWithEmailLink(email, mEmailLink);
-    }
-
+    /**
+     * Sign in using an email address and a link, the link is passed to the Activity
+     * from the dynamic link contained in the email.
+     */
     private void signInWithEmailLink(String email, String link) {
         Log.d(TAG, "signInWithLink:" + link);
 
@@ -245,6 +227,26 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
                 });
     }
 
+    private void onSendLinkClicked() {
+        String email = mEmailField.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            mEmailField.setError("Email must not be empty.");
+            return;
+        }
+
+        sendSignInLink(email);
+    }
+
+    private void onSignInClicked() {
+        String email = mEmailField.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            mEmailField.setError("Email must not be empty.");
+            return;
+        }
+
+        signInWithEmailLink(email, mEmailLink);
+    }
+
     private void onSignOutClicked() {
         mAuth.signOut();
 
@@ -252,4 +254,291 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         mStatusText.setText(R.string.status_email_not_sent);
     }
 
+//    private void updateUI(@Nullable FirebaseUser user) {
+//        if (user != null) {
+//            mStatusText.setText(getString(R.string.passwordless_status_fmt,
+//                    user.getEmail(), user.isEmailVerified()));
+//
+//            findViewById(R.id.mainFields).setVisibility(View.GONE);
+//            findViewById(R.id.passwordlessButtons).setVisibility(View.GONE);
+//            findViewById(R.id.signedInButtons).setVisibility(View.VISIBLE);
+//        } else {
+//            findViewById(R.id.mainFields).setVisibility(View.VISIBLE);
+//            findViewById(R.id.passwordlessButtons).setVisibility(View.VISIBLE);
+//            findViewById(R.id.signedInButtons).setVisibility(View.GONE);
+//        }
+//    }
+
+    private void showSnackbar(String message) {
+        Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.passwordlessSendEmailButton:
+                onSendLinkClicked();
+                break;
+            case R.id.passwordlessSignInButton:
+                onSignInClicked();
+                break;
+            case R.id.signOutButton:
+                onSignOutClicked();
+                break;
+        }
+    }
 }
+//
+//import android.content.Intent;
+//import android.support.annotation.NonNull;
+//import android.support.annotation.Nullable;
+//import android.os.Bundle;
+//import android.text.TextUtils;
+//import android.util.Log;
+//import com.example.kante.live_alone.R;
+//import android.view.View;
+//import android.widget.Button;
+//import android.widget.EditText;
+//import android.widget.TextView;
+//
+//import com.google.android.gms.tasks.OnCompleteListener;
+//import com.google.android.gms.tasks.Task;
+//import com.google.firebase.auth.ActionCodeSettings;
+//import com.google.firebase.auth.AuthResult;
+//import com.google.firebase.auth.FirebaseAuth;
+//import com.google.firebase.auth.FirebaseUser;
+//import android.support.design.widget.Snackbar;
+//import com.google.firebase.auth.FirebaseAuthActionCodeException;
+//import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+//
+//
+//
+//public class MainActivity extends BaseActivity implements View.OnClickListener {
+//    private static final String TAG = "EMAIL_LINK_AUTHENTICATION";
+//    private static final String KEY_PENDING_EMAIL = "key_pending_email";
+//
+//    private FirebaseAuth mAuth;
+//
+//    private Button mSendLinkButton;
+//    private Button mSignInButton;
+//    private Button mSignOutButton;
+//
+//    private EditText mEmailField;
+//    private TextView mStatusText;
+//
+//    private String mPendingEmail;
+//    private String mEmailLink;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        mAuth = FirebaseAuth.getInstance();
+//
+//        mSendLinkButton = findViewById(R.id.passwordlessSendEmailButton);
+//        mSignInButton = findViewById(R.id.passwordlessSignInButton);
+//        mSignOutButton = findViewById(R.id.signOutButton);
+//
+//        mEmailField = findViewById(R.id.fieldEmail);
+//        mStatusText = findViewById(R.id.status);
+//
+//
+//        mSendLinkButton.setOnClickListener(this);
+//        mSignInButton.setOnClickListener(this);
+//        mSignOutButton.setOnClickListener(this);
+//
+//        // Restore the "pending" email address
+//        if (savedInstanceState != null) {
+//            mPendingEmail = savedInstanceState.getString("wltjr0920@ajou.ac.kr", null);
+//            mEmailField.setText(mPendingEmail);
+//        }
+//
+//        // Check if the Intent that started the Activity contains an email sign-in link.
+//        checkIntent(getIntent());
+//
+//    }
+//
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        updateUI(mAuth.getCurrentUser());
+//    }
+//
+//    private void updateUI(@Nullable FirebaseUser user) {
+//        if (user != null) {
+//            mStatusText.setText(getString(R.string.passwordless_status_fmt,
+//                    user.getEmail(), user.isEmailVerified()));
+//
+//            findViewById(R.id.mainFields).setVisibility(View.GONE);
+//            findViewById(R.id.passwordlessButtons).setVisibility(View.GONE);
+//            findViewById(R.id.signedInButtons).setVisibility(View.VISIBLE);
+//            if(user.isEmailVerified()){
+//                Intent intent = new Intent(this,
+//                        AfterLogin.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//        } else {
+//            findViewById(R.id.mainFields).setVisibility(View.VISIBLE);
+//            findViewById(R.id.passwordlessButtons).setVisibility(View.VISIBLE);
+//            findViewById(R.id.signedInButtons).setVisibility(View.GONE);
+//        }
+//    }
+//
+//    @Override
+//    protected void onNewIntent(Intent intent) {
+//        super.onNewIntent(intent);
+//        checkIntent(intent);
+//    }
+//
+//    @Override
+//    protected void onSaveInstanceState(Bundle outState) {
+//        super.onSaveInstanceState(outState);
+//        outState.putString(KEY_PENDING_EMAIL, mPendingEmail);
+//    }
+//
+//    private void checkIntent(@Nullable Intent intent) {
+//        if (intentHasEmailLink(intent)) {
+//            mEmailLink = intent.getData().toString();
+//            mStatusText.setText(R.string.status_link_found);
+//            mSendLinkButton.setEnabled(false);
+//            mSignInButton.setEnabled(true);
+//        } else {
+//            mStatusText.setText(R.string.status_email_not_sent);
+//            mSendLinkButton.setEnabled(true);
+//            mSignInButton.setEnabled(false);
+//        }
+//    }
+//
+//    /**
+//     * Determine if the given Intent contains an email sign-in link.
+//     */
+//    private boolean intentHasEmailLink(@Nullable Intent intent) {
+//        if (intent != null && intent.getData() != null) {
+//            String intentData = intent.getData().toString();
+//            if (mAuth.isSignInWithEmailLink(intentData)) {
+//                return true;
+//            }
+//        }
+//
+//        return false;
+//    }
+//
+//
+//    private void showSnackbar(String message) {
+//        Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_SHORT).show();
+//    }
+//
+//    @Override
+//    public void onClick(View view) {
+//        switch (view.getId()) {
+//            case R.id.passwordlessSendEmailButton:
+//                onSendLinkClicked();
+//                break;
+//            case R.id.passwordlessSignInButton:
+//                onSignInClicked();
+//                break;
+//            case R.id.signOutButton:
+//                onSignOutClicked();
+//                break;
+//        }
+//    }
+//
+//    private void onSendLinkClicked() {
+//        String email = mEmailField.getText().toString();
+//        if (TextUtils.isEmpty(email)) {
+//            mEmailField.setError("Email must not be empty.");
+//            return;
+//        }
+//
+//        sendSignInLink(email);
+//    }
+//
+//    private void sendSignInLink(String email) {
+//        ActionCodeSettings settings = ActionCodeSettings.newBuilder()
+//                .setAndroidPackageName(
+//                        getPackageName(),
+//                        false, /* install if not available? */
+//                        null   /* minimum app version */)
+//                .setHandleCodeInApp(true)
+//                .setUrl("https://www.naver.com/")
+//                .build();
+//
+//        hideKeyboard(mEmailField);
+//        showProgressDialog();
+//
+//        final String email_new=email;
+//        mAuth.sendSignInLinkToEmail(email_new, settings)
+//                .addOnCompleteListener(new OnCompleteListener<Void>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<Void> task) {
+//                        hideProgressDialog();
+//
+//                        if (task.isSuccessful()) {
+//                            Log.d(TAG, "Link sent");
+//                            showSnackbar("Sign-in link sent!");
+//
+//                            mPendingEmail = email_new;
+//                            mStatusText.setText(R.string.status_email_sent);
+//                        } else {
+//                            Exception e = task.getException();
+//                            Log.w(TAG, "Could not send link", e);
+//                            showSnackbar("Failed to send link.");
+//
+//                            if (e instanceof FirebaseAuthInvalidCredentialsException) {
+//                                mEmailField.setError("Invalid email address.");
+//                            }
+//                        }
+//                    }
+//                });
+//    }
+//
+//    private void onSignInClicked() {
+//        String email = mEmailField.getText().toString();
+//        if (TextUtils.isEmpty(email)) {
+//            mEmailField.setError("Email must not be empty.");
+//            return;
+//        }
+//
+//        signInWithEmailLink(email, mEmailLink);
+//    }
+//
+//    private void signInWithEmailLink(String email, String link) {
+//        Log.d(TAG, "signInWithLink:" + link);
+//
+//        hideKeyboard(mEmailField);
+//        showProgressDialog();
+//
+//        mAuth.signInWithEmailLink(email, link)
+//                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<AuthResult> task) {
+//                        hideProgressDialog();
+//                        mPendingEmail = null;
+//
+//                        if (task.isSuccessful()) {
+//                            Log.d(TAG, "signInWithEmailLink:success");
+//
+//                            mEmailField.setText(null);
+//                            updateUI(task.getResult().getUser());
+//                        } else {
+//                            Log.w(TAG, "signInWithEmailLink:failure", task.getException());
+//                            updateUI(null);
+//
+//                            if (task.getException() instanceof FirebaseAuthActionCodeException) {
+//                                showSnackbar("Invalid or expired sign-in link.");
+//                            }
+//                        }
+//                    }
+//                });
+//    }
+//
+//    private void onSignOutClicked() {
+//        mAuth.signOut();
+//
+//        updateUI(null);
+//        mStatusText.setText(R.string.status_email_not_sent);
+//    }
+//
+//}
