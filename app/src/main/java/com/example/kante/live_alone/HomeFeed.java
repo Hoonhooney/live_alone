882187b1d0881a393b4354f594f8ef51dce770e1
kante <kante@popone.co.kr>
2019-05-12 10:23:42 +0900

    홈피드에서 카테고리가 Posting 클래스로 넘어가도록 수정

--- app/src/main/java/com/example/kante/live_alone/HomeFeed.java
+++ app/src/main/java/com/example/kante/live_alone/HomeFeed.java
@@ -1,23 +1,65 @@
 package com.example.kante.live_alone;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class HomeFeed extends AppCompatActivity {
+
+    public static String TAG="HomeFeed";
+
+    private FirebaseFirestore firebaseFirestore;
+    private FirebaseUser firebaseUser;
+    private Fragment fr;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        DocumentReference docRef = firebaseFirestore.collection("users").document(firebaseUser.getUid());
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document != null && document.exists()) {
+                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                    } else {
+                        Intent i = new Intent(HomeFeed.this,EnterDetailed.class);
+                        startActivityForResult(i, 1);
+                        Log.d(TAG, "No such document");
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homefeed);
 
-        Fragment firstFragment = new FCook();
-        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container,firstFragment).commit();
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        fr = new FCook();
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container,fr).commit();
 
         Button bCook = (Button) findViewById(R.id.cook);
         Button bRoom = (Button) findViewById(R.id.room);
@@ -33,7 +75,7 @@ public class HomeFeed extends AppCompatActivity {
     }
 
     public void selectCategory(View view){
-        Fragment fr = null;
+        fr = null;
         switch(view.getId()){
             case R.id.cook:
                 fr = new FCook();
@@ -55,6 +97,7 @@ public class HomeFeed extends AppCompatActivity {
     }
     private void goPosting(){
         Intent intent = new Intent(this, Posting.class);
+        intent.putExtra("Category",fr.getClass().getSimpleName());
         startActivity(intent);
     }
 }
